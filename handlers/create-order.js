956804c9f25966b358e225e183188ce2210485b0
Rolando Sotelo <rola@hey.com>
2022-01-27 00:42:45 -0600

    🎉 NEW FEATURE: Put order into dynamodb with aws-sdk

--- handlers/create-order.js
+++ handlers/create-order.js
@@ -1,10 +1,31 @@
+const AWS = require("aws-sdk");
+const docClient = new AWS.DynamoDB.DocumentClient();
+
 function createOrder(order) {
   if (!order || !order.itemId || !order.address)
     throw new Error(
       "To order your dinner please provide the item and address where it should be delivered."
     );
 
-  return {};
+  return docClient
+    .put({
+      TableName: "dinner-orders",
+      Item: {
+        orderId: "some-id",
+        item: order.itemId,
+        address: order.address,
+        orderStatus: "pending",
+      },
+    })
+    .promise()
+    .then((res) => {
+      console.log("Order is saved!", res);
+      return res;
+    })
+    .catch((saveError) => {
+      console.log("Ops order is not saved :(", saveError);
+      throw saveError;
+    });
 }
 
 module.exports = createOrder;
