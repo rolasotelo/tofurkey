8b20da90f50236a522e27f0b1e9cc609684dab59
Rolando Sotelo <rola@hey.com>
2022-01-31 23:33:57 -0600

    👌 IMPROVE: Update order updates table indynamodb

--- handlers/update-order.js
+++ handlers/update-order.js
@@ -1,10 +1,31 @@
+const AWS = require("aws-sdk");
+const docClient = new AWS.DynamoDB.DocumentClient();
+
 function updateOrder({ order, orderId }) {
   if (!order || !orderId)
     throw new Error(
       "To update your dinner please provide the orderId and updated order."
     );
 
-  return { message: `Updated order ${orderId}` };
+  return docClient
+    .put({
+      TableName: "dinner-orders",
+      Item: {
+        orderId: orderId,
+        item: order.itemId,
+        address: order.address,
+        orderStatus: "pending",
+      },
+    })
+    .promise()
+    .then((res) => {
+      console.log("Order was updated!", res);
+      return res;
+    })
+    .catch((saveError) => {
+      console.log("Ops order was not updated :(", saveError);
+      throw saveError;
+    });
 }
 
 module.exports = updateOrder;
