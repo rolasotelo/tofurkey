76218dc54327a31d8ccb53deea6b8bcbb02fe8cb
Rolando Sotelo <rola@hey.com>
2022-01-31 23:43:35 -0600

    👌 IMPROVE: Delete order in dynamodb

--- handlers/delete-order.js
+++ handlers/delete-order.js
@@ -1,8 +1,26 @@
+const AWS = require("aws-sdk");
+const docClient = new AWS.DynamoDB.DocumentClient();
+
 function deleteOrder(orderId) {
   if (!orderId)
     throw new Error("To delete an order please provide a valid orderId");
 
-  return { message: `Order ${orderId} deleted.` };
+  return docClient
+    .delete({
+      TableName: "dinner-orders",
+      Key: {
+        orderId: orderId,
+      },
+    })
+    .promise()
+    .then((res) => {
+      console.log("Order was deleted!", res);
+      return res;
+    })
+    .catch((saveError) => {
+      console.log("Ops order was not deleted :(", saveError);
+      throw saveError;
+    });
 }
 
 module.exports = deleteOrder;
